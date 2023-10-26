b15a1dac7aa8e00f0eeee05c658113403d550af9
HHSong1204 <hhsong1204@gmail.com>
2023-10-26 11:09:19 +0900

    [add] XcFramework 추가

---
+++ Sources/AdMovie_O/AdMovie_O.xcframework/ios-arm64_x86_64-simulator/HHSong_AdMovie.framework/Headers/HHSong_AdMovie.h
@@ -0,0 +1,18 @@
+//
+//  HHSong_AdMovie.h
+//  HHSong_AdMovie
+//
+//  Created by 송황호 on 2023/10/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HHSong_AdMovie.
+FOUNDATION_EXPORT double HHSong_AdMovieVersionNumber;
+
+//! Project version string for HHSong_AdMovie.
+FOUNDATION_EXPORT const unsigned char HHSong_AdMovieVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HHSong_AdMovie/PublicHeader.h>
+
+
